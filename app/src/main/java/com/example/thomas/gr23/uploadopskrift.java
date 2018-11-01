c83b079266bbdc5fd87778ee316642c981b422c2
theshadowmuffin <christoffer@lwp.dk>
2018-11-01 19:39:26 +0100

    Omdøbt store for ikke at blande filer sammen

--- app/src/main/java/com/example/thomas/gr23/uploadopskrift.java
+++ app/src/main/java/com/example/thomas/gr23/uploadopskrift.java
@@ -1,4 +1,20 @@
 package com.example.thomas.gr23;
 
-public class uploadopskrift {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class uploadopskrift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploadopskrift);
+
+
+
+    }
+
+
+
+
 }
