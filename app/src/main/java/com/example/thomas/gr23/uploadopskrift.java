5ba00121da4ea3264ade066225b8e1e1f64548a2
theshadowmuffin <christoffer@lwp.dk>
2018-11-08 14:13:03 +0100

    Tilføjet en spinner

--- app/src/main/java/com/example/thomas/gr23/uploadopskrift.java
+++ app/src/main/java/com/example/thomas/gr23/uploadopskrift.java
@@ -2,6 +2,8 @@ package com.example.thomas.gr23;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class uploadopskrift extends AppCompatActivity {
 
@@ -10,7 +12,10 @@ public class uploadopskrift extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploadopskrift);
 
-
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.svaehedsgrad, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
     }
 
