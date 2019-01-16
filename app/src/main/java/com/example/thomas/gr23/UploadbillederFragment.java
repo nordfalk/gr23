b3f2e9705aa84f529cc7559b1601837f955967b1
Chris <christoffer@lwp.dk>
2019-01-16 08:13:50 +0100

    Startet implementation af fragmenter

---
+++ app/src/main/java/com/example/thomas/gr23/UploadbillederFragment.java
@@ -0,0 +1,22 @@
+package com.example.thomas.gr23;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UploadbillederFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_opskriftbbilleder, container, false);
+
+
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/thomas/gr23/UploadopskriftFragment.java
@@ -0,0 +1,24 @@
+package com.example.thomas.gr23;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UploadopskriftFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_uploadopskrift, container, false);
+
+
+
+
+        return view;
+    }
+
+
+}
