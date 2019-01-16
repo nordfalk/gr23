0ff79f289cc9d151087cfbb51f5600a63d82139c
Chris <christoffer@lwp.dk>
2019-01-16 05:54:27 +0100

    Startet på upload logik, startet på fragmenter til oploadopskrift.

---
+++ app/src/main/java/com/example/thomas/gr23/uploadopskriftlogik.java
@@ -0,0 +1,11 @@
+package com.example.thomas.gr23;
+
+import android.media.Image;
+
+public class uploadopskriftlogik {
+    private String navn, garn, pindenaale, extra;
+    private int kategori, type, pris, svaerhedsgrad;
+    private Image billed1, billed2, billed3, billed4, billed5, billed6;
+    private String[] trin;
+
+}
