f590ccc49a30dcf8534fa1127cc49687e2674489
theshadowmuffin <christoffer@lwp.dk>
2019-01-16 11:12:40 +0100

    Ændring af app navnet

--- app/src/main/java/com/example/thomas/gr23/OpskriftFragment.java
+++ app/src/main/java/com/example/thomas/gr23/OpskriftFragment.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class OpskriftFragment extends Fragment {
+public class OpskriftFragment extends Fragment implements View.OnClickListener {
+    Button naeste;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -14,10 +17,25 @@ public class OpskriftFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_opskrift, container, false);
 
-
+        naeste = view.findViewById(R.id.naeste);
+        naeste.setOnClickListener(this);
 
 
         return view;
     }
 
+    public void showOtherFragment(){
+        Fragment fr=new UploadbillederFragment();
+        FragmentChangeListener fc=(FragmentChangeListener)getActivity();
+        fc.replaceFragment(fr);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == naeste){
+            //todo sørg for at gemme dataen i fragmentet
+            showOtherFragment();
+
+        }
+    }
 }
--- app/src/main/java/com/example/thomas/gr23/UploadopskriftFragment.java
+++ app/src/main/java/com/example/thomas/gr23/UploadopskriftFragment.java
@@ -5,9 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class UploadopskriftFragment extends Fragment {
-
+public class UploadopskriftFragment extends Fragment implements View.OnClickListener {
+    Button naeste;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,16 +16,27 @@ public class UploadopskriftFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_uploadopskrift, container, false);
 
-
+        naeste = view.findViewById(R.id.naeste);
+        naeste.setOnClickListener(this);
 
 
         return view;
     }
 
-//    public void showOtherFragment()
-//    {
-//        Fragment fr=new NewDisplayingFragment();
-//        FragmentChangeListener fc=(FragmentChangeListener)getActivity();
-//        fc.replaceFragment(fr);
-//    }
+    @Override
+    public void onClick(View v) {
+        if (v == naeste) {
+            //todo sørg for at gemme dataen i fragmentet
+            showOtherFragment();
+        }
+    }
+
+
+
+    public void showOtherFragment(){
+        Fragment fr=new OpskriftFragment();
+        FragmentChangeListener fc=(FragmentChangeListener)getActivity();
+        fc.replaceFragment(fr);
+    }
+
 }
