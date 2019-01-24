b87680ac6021e8bc90fa51625901b387b8fd04a9
Chris <christoffer@lwp.dk>
2019-01-24 21:54:26 +0100

    Oprettet activitet der skal indeholde videoer

--- app/src/main/java/com/example/thomas/gr23/Strikketeknikker.java
+++ app/src/main/java/com/example/thomas/gr23/Strikketeknikker.java
@@ -1,10 +1,23 @@
 package com.example.thomas.gr23;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 public class Strikketeknikker extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strikketeknikker);
 
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
 
+//        videoView.setVideoPath("");
 
+        videoView.start();
+    }
 }
+
+
+
